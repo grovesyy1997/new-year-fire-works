fd65cca38838709230f6e27f16026624b9243ccc
grovesyy1997 <85640183+grovesyy1997@users.noreply.github.com>
2025-10-05 16:07:56 +0100

    date updated

--- server.lua
+++ server.lua
@@ -11,14 +11,17 @@ CreateThread(function()
     while true do
         Wait(60000) -- Check every minute
         local time = GetTime()
-        if time.h == 0 and time.m == 0 then
+        -- Only run on January 1st at 00:00
+        local date = os.date("*t")
+        if date.day == 1 and date.month == 1 and time.h == 0 and time.m == 0 then
             FireworkShow()
-        elseif time.h == 0 and time.m == 15 then
+        elseif date.day == 1 and date.month == 1 and time.h == 0 and time.m == 15 then
             print("^1Fireworkshow is over.^0")
             TriggerClientEvent("firework:stopFireworkShow", -1)
         end
     end
 end)
+
 
 local function FireworkShow()
     print("It's new year! Time to shine. Starting Fireworkshow....")
